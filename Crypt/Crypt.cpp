9117f9dbeeafebf767208512f687a34a843992eb
Drew kestell <drew.kestell@gmail.com>
2020-06-21 11:50:22 -0700

    WIP

--- Crypt/Crypt.cpp
+++ Crypt/Crypt.cpp
@@ -1,11 +1,101 @@
+#include <Windows.h>
+#include <wincrypt.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[])
 {
-    printf("%i", argc);
+    HCRYPTPROV hCryptProv;
+    HCRYPTHASH hHash;
+    HCRYPTKEY hKey;
 
-    for (auto i = 0; i < argc; i++)
-    {
-        printf(argv[i]);
-    }
+    CryptAcquireContext(
+        &hCryptProv,
+        NULL,
+        NULL,
+        PROV_RSA_FULL,
+        0);
+
+    CryptCreateHash(
+        hCryptProv,
+        CALG_MD5,
+        0,
+        0,
+        &hHash);
+
+    CryptHashData(
+        hHash,
+        (BYTE*)argv[1],
+        strlen(argv[1]),
+        CRYPT_USERDATA
+    );
+
+    CryptDeriveKey(
+        hCryptProv,
+        CALG_RC2,
+        hHash,
+        CRYPT_NO_SALT,
+        &hKey);
+
+    DWORD dwHashLen = strlen(argv[3]);
+    auto res = CryptEncrypt(
+        hKey,
+        NULL,
+        true,
+        0,
+        NULL,
+        &dwHashLen,
+        NULL);
+
+    BYTE* buffer = new BYTE[dwHashLen];
+    ZeroMemory(buffer, dwHashLen);
+    memcpy(buffer, argv[3], strlen(argv[3]));
+
+    DWORD dataLength = strlen(argv[3]);
+    CryptEncrypt(
+        hKey,
+        NULL,
+        true,
+        0,
+        buffer,
+        &dataLength,
+        dwHashLen);
+
+    CryptDestroyKey(hKey);
+    CryptDestroyHash(hHash);
+    CryptReleaseContext(hCryptProv, 0);
+
+    HANDLE hFile;
+    hFile = CreateFileA(
+        argv[2],
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    DWORD bytesWritten;
+    WriteFile(
+        hFile,
+        buffer,
+        dataLength,
+        &bytesWritten,
+        NULL
+    );
+
+    /*SetFilePointer(
+        hFile,
+        32,
+        NULL,
+        0
+    );
+
+    WriteFile(
+        hFile,
+        pbHash,
+        dwHashLen,
+        &bytesWritten,
+        NULL
+    );*/
 }
